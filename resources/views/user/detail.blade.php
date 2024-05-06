15f8976dcedb8973f5667aec608046c7e8c029e6
idn-fahmifn <fahmifn.idn@gmail.com>
2024-05-06 11:11:17 +0700

    card-2

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -8,35 +8,48 @@ Detail {{$user->name}}
 {{-- Area Detai Pemilik Toko --}}
 <div class="row">
     <div class="col-md-12 col-sm-12">
+        {{-- show data card --}}
         <div class="card">
             <div class="card-header">
                 <div class="card-title">
-                    <h3>Detail User</h3>
+                    <h5>Detail User</h5>
                 </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <tr>
+                            <th>Nama Lengkap</th>
+                            <td width="5%"> : </td>
+                            <td width="70%">{{$user->name}}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td width="5%"> : </td>
+                            <td width="70%">{{$user->email}}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        {{-- card-edit --}}
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Edit Data</h3>
+
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
                         <i class="fas fa-minus"></i>
                     </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                    </button>
                 </div>
             </div>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-borderless">
-                    <tr>
-                        <th>Nama Lengkap</th>
-                        <td width="5%"> : </td>
-                        <td width="70%">{{$user->name}}</td>
-                    </tr>
-                    <tr>
-                        <th>Email</th>
-                        <td width="5%"> : </td>
-                        <td width="70%">{{$user->email}}</td>
-                    </tr>
-                </table>
+            <!-- /.card-header -->
+            <div class="card-body">
             </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
