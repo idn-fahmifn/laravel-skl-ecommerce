01f58027aa522ab7318d612bd973e01bd4346ee6
idn-fahmifn <fahmifn.idn@gmail.com>
2024-05-07 13:25:37 +0700

    edit

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -32,12 +32,10 @@ Detail {{$user->name}}
                 </div>
             </div>
         </div>
-
         {{-- card-edit --}}
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">Edit Data</h3>
-
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
                         <i class="fas fa-minus"></i>
@@ -46,8 +44,27 @@ Detail {{$user->name}}
                     </button>
                 </div>
             </div>
-            <!-- /.card-header -->
             <div class="card-body">
+                <form action="{{route('penjual.store')}}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label>Nama Lengkap Penjual</label>
+                        <input type="text" name="name" value="{{$user->name}}" required class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Alamat Email</label>
+                        <input type="email" name="email" value="{{$user->email}}" required class="form-control">
+                        <input type="text" name="level" hidden value="penjual">
+                    </div>
+                    <div class="form-group">
+                        <label>Katasandi</label>
+                        <input type="password" name="password" required class="form-control"
+                            placeholder="Minimal 8 karakter, A-Z, a-z dan simbol">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
